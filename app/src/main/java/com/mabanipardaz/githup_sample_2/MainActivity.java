b3256830debcdba68e95505fa10520d135f62f0b
Reza_Rep <rezaaaghasemi@gmail.com>
2018-06-27 16:41:53 +0430

    Initial commit

---
+++ app/src/main/java/com/mabanipardaz/githup_sample_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mabanipardaz.githup_sample_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
